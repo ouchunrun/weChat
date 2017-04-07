6f53c8787b32b6247f50489b200141b6efb09a77
王志文 <w_angzhiwen@163.com>
2017-04-08 00:19:40 +0800

    修改数据模型，

--- Account/models.py
+++ Account/models.py
@@ -1,12 +1,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from message.models import User
 
 
 class Spend(models.Model):
     money = models.FloatField(db_column="money")
     useTime = models.DateTimeField(db_column="useTime")
     useThing = models.CharField(max_length=150, db_column="useThing")
+    userId = models.ForeignKey(User, db_column="user")
 
     class Meta:
         db_table = 'Spend'
@@ -15,6 +17,7 @@ class Spend(models.Model):
 class Bill(models.Model):
     account = models.FloatField(db_column="account")
     createDate = models.DateTimeField(db_column="creatDate")
+    userId = models.ForeignKey(User, db_column="user")
 
     class Meta:
         db_table = 'Bill'
